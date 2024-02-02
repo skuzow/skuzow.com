864b41752e0827ff06b4cf6c673526e7e5877dd3
skuzow <alex@skuzow.net>
2024-02-03 00:48:10 +0100

    feat: header & tailwind colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: '#0D0D0D',
+        primary: '#FFFFFF',
+        secondary: '#FAFF00'
+      },
+      fontFamily: {
+        header: ['Geist Mono Medium', 'system-ui', 'sans-serif'],
+        title: ['Geist Medium', 'system-ui', 'sans-serif']
+      }
+    }
   },
   plugins: []
 };
