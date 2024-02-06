6c8d26d6e1635d5a299a78d20be07dbed2ad3a5f
skuzow <alex@skuzow.net>
2024-02-06 13:54:28 +0100

    feat: welcome menu

---
+++ src/constants/navItems.ts
@@ -0,0 +1,14 @@
+export const navItems = [
+  {
+    label: 'about',
+    url: '/#about'
+  },
+  {
+    label: 'projects',
+    url: '/#projects'
+  },
+  {
+    label: 'contact',
+    url: '/#contact'
+  }
+];
