94deb7d6d82bffd3c24443f001d79103f46bc5fe
skuzow <alex@skuzow.net>
2024-02-09 22:26:20 +0100

    feat: scrolldown marquee spinner

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,20 @@ export default {
       fontFamily: {
         header: ['Geist Mono Medium', 'system-ui', 'sans-serif'],
         title: ['Geist Medium', 'system-ui', 'sans-serif']
+      },
+      animation: {
+        marquee1: 'marquee1 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite'
+      },
+      keyframes: {
+        marquee1: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(100%)' }
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0%)' }
+        }
       }
     }
   },
