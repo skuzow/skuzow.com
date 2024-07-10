1d19f94b2fd62ad7abb3f16272d57c73f716dd9c
skuzow <alex@skuzow.net>
2024-07-10 21:10:04 +0200

    feat: setup shadcn/ui vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    vue(),
+    tailwind({
+      applyBaseStyles: false
+    })
+  ]
 });
