a952af8ef45acc5122b299b91157dae7cb76a21e
skuzow <alex@skuzow.net>
2024-07-13 23:17:13 +0200

    feat: sections init

---
+++ src/constants/sectionItems.ts
@@ -0,0 +1,19 @@
+export interface SectionItem {
+  url: string;
+  label: string;
+}
+
+export const SECTION_ITEMS: SectionItem[] = [
+  {
+    url: '/#projects',
+    label: 'projects'
+  },
+  {
+    url: '/#experience',
+    label: 'experience'
+  },
+  {
+    url: '/#posts',
+    label: 'posts'
+  }
+];
