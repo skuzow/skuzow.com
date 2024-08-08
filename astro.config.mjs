9df724a0724cb7e50daab862ebb1e6f229cfb7ef
skuzow <alex@skuzow.net>
2024-08-08 23:30:34 +0200

    feat: sitemap & prioritize order head attributes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://skuzow.com',
   integrations: [
     vue(),
+    sitemap(),
     tailwind({
       applyBaseStyles: false
     })
