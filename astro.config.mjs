438b6ba6ba7a5c4e9b8cd9b788975f0ecc715c13
skuzow <alex@skuzow.net>
2024-08-14 02:29:51 +0200

    feat: vercel hybrid page mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel/serverless';
+
 import vue from '@astrojs/vue';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
@@ -7,6 +9,8 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://skuzow.com',
+  output: 'hybrid',
+  adapter: vercel(),
   integrations: [
     vue(),
     sitemap(),
