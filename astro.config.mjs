898907af6780fac6d452f8642c5392d6a17d4819
skuzow <alex@skuzow.net>
2025-07-02 22:35:11 +0200

    feat: update tailwind & shadcn vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 import vue from '@astrojs/vue';
 import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://skuzow.com',
-  integrations: [
-    vue(),
-    sitemap(),
-    tailwind({
-      applyBaseStyles: false
-    })
-  ]
+  vite: {
+    plugins: [
+      tailwindcss({
+        applyBaseStyles: false
+      })
+    ]
+  },
+  integrations: [vue(), sitemap()]
 });
