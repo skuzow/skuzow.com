5e4f9db04d20adf233f62bd3687c7c710421ecf7
skuzow <alex@skuzow.net>
2025-07-04 18:23:56 +0200

    feat(ui): update shadcn vue components

--- src/components/ui/dropdown-menu/index.ts
+++ src/components/ui/dropdown-menu/index.ts
@@ -1,6 +1,6 @@
-export { DropdownMenuPortal } from 'radix-vue';
+export { DropdownMenuPortal } from 'reka-ui';
 
 export { default as DropdownMenu } from './DropdownMenu.vue';
-export { default as DropdownMenuTrigger } from './DropdownMenuTrigger.vue';
 export { default as DropdownMenuContent } from './DropdownMenuContent.vue';
 export { default as DropdownMenuItem } from './DropdownMenuItem.vue';
+export { default as DropdownMenuTrigger } from './DropdownMenuTrigger.vue';
