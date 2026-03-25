0783f1f2c512ad4ae651c4ec41cf4d6f1a3260d9
skuzow <alex@skuzow.net>
2026-03-26 00:45:14 +0100

    feat(i18n): only use lang

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -3,18 +3,14 @@ import { getRelativeLocaleUrl } from 'astro:i18n';
 import { ui, defaultLang } from './ui';
 
 export function useLang(lang: string | undefined) {
-  return lang || defaultLang;
+  return (lang && lang in ui ? lang : defaultLang) as keyof typeof ui;
 }
 
 export function useTranslations(lang: string | undefined) {
-  return ui[getSelectedLang(lang)];
+  return ui[useLang(lang)];
 }
 
 export function useLocaleUrl(locale: string | undefined) {
-  const lang = getSelectedLang(locale);
+  const lang = useLang(locale);
   return (path: string) => getRelativeLocaleUrl(lang, path);
-}
-
-function getSelectedLang(lang: string | undefined) {
-  return (lang && lang in ui ? lang : defaultLang) as keyof typeof ui;
 }
